4803545a340349540aeac9e8d734ef1797801837
MYPC <MYPC@DESKTOP-38G0JLO>
2021-06-03 22:30:05 +0800

    Final Design

--- Contact Tracing app/Form1.Designer.cs
+++ Contact Tracing app/Form1.Designer.cs
@@ -29,21 +29,247 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Title = new System.Windows.Forms.Label();
+            this.nametxttbx = new System.Windows.Forms.TextBox();
+            this.agetxtbx = new System.Windows.Forms.TextBox();
+            this.addresstxtbx = new System.Windows.Forms.TextBox();
+            this.namelab = new System.Windows.Forms.Label();
+            this.agelab = new System.Windows.Forms.Label();
+            this.Addresslab = new System.Windows.Forms.Label();
+            this.postaltxtbx = new System.Windows.Forms.TextBox();
+            this.postallab = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.contactlab = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.datelab = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.timelab = new System.Windows.Forms.Label();
+            this.submitbtn = new System.Windows.Forms.Button();
+            this.resetbtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Title.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(1, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(253, 31);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Contact Tracing Form";
+            // 
+            // nametxttbx
+            // 
+            this.nametxttbx.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nametxttbx.Location = new System.Drawing.Point(6, 86);
+            this.nametxttbx.Name = "nametxttbx";
+            this.nametxttbx.Size = new System.Drawing.Size(191, 20);
+            this.nametxttbx.TabIndex = 1;
+            // 
+            // agetxtbx
+            // 
+            this.agetxtbx.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.agetxtbx.Location = new System.Drawing.Point(6, 145);
+            this.agetxtbx.Name = "agetxtbx";
+            this.agetxtbx.Size = new System.Drawing.Size(69, 20);
+            this.agetxtbx.TabIndex = 2;
+            // 
+            // addresstxtbx
+            // 
+            this.addresstxtbx.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addresstxtbx.Location = new System.Drawing.Point(6, 198);
+            this.addresstxtbx.Name = "addresstxtbx";
+            this.addresstxtbx.Size = new System.Drawing.Size(216, 20);
+            this.addresstxtbx.TabIndex = 3;
+            // 
+            // namelab
+            // 
+            this.namelab.AutoSize = true;
+            this.namelab.BackColor = System.Drawing.Color.Transparent;
+            this.namelab.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.namelab.Location = new System.Drawing.Point(3, 61);
+            this.namelab.Name = "namelab";
+            this.namelab.Size = new System.Drawing.Size(56, 22);
+            this.namelab.TabIndex = 4;
+            this.namelab.Text = "Name";
+            // 
+            // agelab
+            // 
+            this.agelab.AutoSize = true;
+            this.agelab.BackColor = System.Drawing.Color.Transparent;
+            this.agelab.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.agelab.Location = new System.Drawing.Point(3, 120);
+            this.agelab.Name = "agelab";
+            this.agelab.Size = new System.Drawing.Size(42, 22);
+            this.agelab.TabIndex = 5;
+            this.agelab.Text = "Age";
+            // 
+            // Addresslab
+            // 
+            this.Addresslab.AutoSize = true;
+            this.Addresslab.BackColor = System.Drawing.Color.Transparent;
+            this.Addresslab.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Addresslab.Location = new System.Drawing.Point(3, 173);
+            this.Addresslab.Name = "Addresslab";
+            this.Addresslab.Size = new System.Drawing.Size(76, 22);
+            this.Addresslab.TabIndex = 6;
+            this.Addresslab.Text = "Address";
+            // 
+            // postaltxtbx
+            // 
+            this.postaltxtbx.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postaltxtbx.Location = new System.Drawing.Point(242, 198);
+            this.postaltxtbx.Name = "postaltxtbx";
+            this.postaltxtbx.Size = new System.Drawing.Size(69, 20);
+            this.postaltxtbx.TabIndex = 7;
+            // 
+            // postallab
+            // 
+            this.postallab.AutoSize = true;
+            this.postallab.BackColor = System.Drawing.Color.Transparent;
+            this.postallab.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postallab.Location = new System.Drawing.Point(238, 173);
+            this.postallab.Name = "postallab";
+            this.postallab.Size = new System.Drawing.Size(59, 22);
+            this.postallab.TabIndex = 8;
+            this.postallab.Text = "Postal";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(6, 256);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(144, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // contactlab
+            // 
+            this.contactlab.AutoSize = true;
+            this.contactlab.BackColor = System.Drawing.Color.Transparent;
+            this.contactlab.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contactlab.Location = new System.Drawing.Point(3, 231);
+            this.contactlab.Name = "contactlab";
+            this.contactlab.Size = new System.Drawing.Size(138, 22);
+            this.contactlab.TabIndex = 10;
+            this.contactlab.Text = "Contact Number";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(6, 314);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(144, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // datelab
+            // 
+            this.datelab.AutoSize = true;
+            this.datelab.BackColor = System.Drawing.Color.Transparent;
+            this.datelab.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datelab.Location = new System.Drawing.Point(3, 288);
+            this.datelab.Name = "datelab";
+            this.datelab.Size = new System.Drawing.Size(47, 22);
+            this.datelab.TabIndex = 12;
+            this.datelab.Text = "Date";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(167, 314);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(69, 20);
+            this.textBox7.TabIndex = 15;
+            // 
+            // timelab
+            // 
+            this.timelab.AutoSize = true;
+            this.timelab.BackColor = System.Drawing.Color.Transparent;
+            this.timelab.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timelab.Location = new System.Drawing.Point(163, 288);
+            this.timelab.Name = "timelab";
+            this.timelab.Size = new System.Drawing.Size(50, 22);
+            this.timelab.TabIndex = 16;
+            this.timelab.Text = "Time";
+            // 
+            // submitbtn
+            // 
+            this.submitbtn.BackColor = System.Drawing.SystemColors.Window;
+            this.submitbtn.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitbtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.submitbtn.Location = new System.Drawing.Point(242, 355);
+            this.submitbtn.Name = "submitbtn";
+            this.submitbtn.Size = new System.Drawing.Size(91, 35);
+            this.submitbtn.TabIndex = 17;
+            this.submitbtn.Text = "Submit";
+            this.submitbtn.UseVisualStyleBackColor = false;
+            // 
+            // resetbtn
+            // 
+            this.resetbtn.BackColor = System.Drawing.SystemColors.Window;
+            this.resetbtn.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetbtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.resetbtn.Location = new System.Drawing.Point(145, 355);
+            this.resetbtn.Name = "resetbtn";
+            this.resetbtn.Size = new System.Drawing.Size(91, 35);
+            this.resetbtn.TabIndex = 18;
+            this.resetbtn.Text = "Reset";
+            this.resetbtn.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
             this.ClientSize = new System.Drawing.Size(342, 402);
+            this.Controls.Add(this.resetbtn);
+            this.Controls.Add(this.submitbtn);
+            this.Controls.Add(this.timelab);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.datelab);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.contactlab);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.postallab);
+            this.Controls.Add(this.postaltxtbx);
+            this.Controls.Add(this.Addresslab);
+            this.Controls.Add(this.agelab);
+            this.Controls.Add(this.namelab);
+            this.Controls.Add(this.addresstxtbx);
+            this.Controls.Add(this.agetxtbx);
+            this.Controls.Add(this.nametxttbx);
+            this.Controls.Add(this.Title);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Contact Tracing Form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.TextBox nametxttbx;
+        private System.Windows.Forms.TextBox agetxtbx;
+        private System.Windows.Forms.TextBox addresstxtbx;
+        private System.Windows.Forms.Label namelab;
+        private System.Windows.Forms.Label agelab;
+        private System.Windows.Forms.Label Addresslab;
+        private System.Windows.Forms.TextBox postaltxtbx;
+        private System.Windows.Forms.Label postallab;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label contactlab;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label datelab;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label timelab;
+        private System.Windows.Forms.Button submitbtn;
+        private System.Windows.Forms.Button resetbtn;
     }
 }
 
